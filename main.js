366d0bf9b4a89987750a10a750725c9ae5856999
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-10-12 21:04:14 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+/* Stwórz dokument HTML. Stwórz plik main.js w odpowiednim folderze i podepnij go pod plik HTML.
+
+W pliku html wewnątrz body dodaj pusty paragraf oraz podepnij Twój skrypt main.js.
+
+3 . W pliku main.js stworz zmienną o dowolnej nazwie i przypisz do niej stworzony wcześniej paragraf (przy użyciu document.querySelector).
+
+Następnie dodaj do paragrafu dowolny tekst wykorzystując właściwość textContent
+
+Pliki dodaj do zdalnego repozytorium i podeślij link do sprawdzenia. */
